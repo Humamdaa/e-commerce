38557630b9cf09364b3d2941027ed7d575f4727e
Humamdaa <homamdaas11@gmail.com>
2025-07-12 23:46:50 +0300

    create cards to show products

--- src/components/MainContext.tsx
+++ src/components/MainContext.tsx
@@ -1,3 +1,134 @@
-const MainContext = ()=>{
-    return 
-}
+import { Tally3 } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useFilter } from './filters';
+import axios from 'axios';
+import BookCard from './BookCard';
+
+const MainContext = () => {
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } =
+    useFilter();
+  const [products, setProducts] = useState<any[]>([]);
+  const [filter, setFilter] = useState('all');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const itemsPerPage = 12;
+
+  useEffect(() => {
+    let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${
+      (currentPage - 1) * itemsPerPage
+    }`;
+    if (keyword) {
+      url = `https://dummyjson.com/products/search?q=${keyword}`;
+     }
+    
+    axios
+      .get(url)
+      .then((response) => {
+        setProducts(response.data.products);
+        console.log(response.data.products);
+      })
+      .catch((error) => {
+        console.error('Error fetching data ', error);
+      });
+  }, [currentPage, keyword]);
+
+  const getFiltersProducts = () => {
+    let filterProducts = products;
+
+    if (selectedCategory) {
+      filterProducts = filterProducts.filter(
+        (product) => product.category === selectedCategory
+      );
+
+      console.log(filterProducts);
+    }
+    if (minPrice !== undefined) {
+      filterProducts = filterProducts.filter(
+        (product) => product.price >= minPrice
+      );
+    }
+
+    if (maxPrice !== undefined) {
+      filterProducts = filterProducts.filter(
+        (product) => product.price <= maxPrice
+      );
+    }
+
+    if (searchQuery) {
+      filterProducts = filterProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    switch (filter) {
+      case 'expensive':
+        return filterProducts.sort((a, b) => b.price - a.price);
+
+      case 'cheap':
+        return filterProducts.sort((a, b) => a.price - b.price);
+      case 'popular':
+        return filterProducts.sort((a, b) => b.rating - a.rating);
+      default:
+        return filterProducts;
+    }
+  };
+
+  const filterProducts = getFiltersProducts();
+  console.log(filterProducts);
+  return (
+    <section className="xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5">
+      <div className="wb-5">
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          <div className="relative mb-5 mt-5">
+            <button className="border px-4 py-2 rounded-full flex items-cneter">
+              <Tally3 className="mr-2" />
+
+              {filter === 'all'
+                ? 'Filter'
+                : filter.charAt(0).toLowerCase() + filter.slice(1)}
+            </button>
+
+            {dropdownOpen && (
+              <div className="absolute bg-white border border-gray-300 rounded mt-2 w-full sm:w-40">
+                <button
+                  onClick={() => setFilter('cheap')}
+                  className="black px-2 w-full text-left hober:bg-gray-200"
+                >
+                  Cheap
+                </button>
+                <button
+                  onClick={() => setFilter('expensive')}
+                  className="black px-2 w-full text-left hober:bg-gray-200"
+                >
+                  Expensive
+                </button>
+                <button
+                  onClick={() => setFilter('popular')}
+                  className="black px-2 w-full text-left hober:bg-gray-200"
+                >
+                  Popular
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 sm:grid-cols-3 md:grid-cols-3 md:grid-cols-4 gap-5">
+          {/* Book card */}
+          {filterProducts.map((product) => (
+            // Book card
+            <BookCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              image={product.thumbnail}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainContext;
